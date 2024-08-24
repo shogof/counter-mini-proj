ee974f14b1b68f5448ad33fe991f6d20d5c650bb
Shegofa <shogofadeveloper12@gmail.com>
2024-08-24 13:26:05 +0400

    responsiveness

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import Button from './Button';
-import './Counter.css';
+import { useState } from "react";
+import Button from "./Button";
+import "./Counter.css";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
@@ -14,9 +14,13 @@ const Counter = () => {
       <h1>Counter</h1>
       <div className="container">
         <h1>{count}</h1>
-        {count > 0 && <Button text="decrease" clickHandler={decreaseHandler} />}
-        <Button text="increase" clickHandler={increaseHandler} />
-        {count > 0 && <Button text="reset" clickHandler={resetHandler} />}
+        <div className="btn">
+          {count > 0 && (
+            <Button text="decrease" clickHandler={decreaseHandler} />
+          )}
+          <Button text="increase" clickHandler={increaseHandler} />
+          {count > 0 && <Button text="reset" clickHandler={resetHandler} />}
+        </div>
       </div>
     </>
   );
