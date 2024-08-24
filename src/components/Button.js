2b9965673270ed157af6e1e6b87e1bdb2772e159
Shegofa <shogofadeveloper12@gmail.com>
2024-08-24 11:09:06 +0400

    button.js added

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import "./Button.css";
+function Button({ text, clickHandler }) {
+  return (
+    <button onClick={clickHandler} className={text}>
+      {text}
+    </button>
+  );
+}
+export default Button;
--- src/components/counter.js
+++ src/components/counter.js
@@ -10,6 +10,15 @@ const Counter = () => {
   const resetHandler = () => setCount(0);
 
   return (
+    <>
+      <h1>Counter</h1>
+      <div className="container">
+        <h1>{count}</h1>
+        {count > 0 && <Button text="decrease" clickHandler={decreaseHandler} />}
+        <Button text="increase" clickHandler={increaseHandler} />
+        {count > 0 && <Button text="reset" clickHandler={resetHandler} />}
+      </div>
+    </>
   );
 };
 
