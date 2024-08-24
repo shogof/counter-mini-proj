53ef11b537db296b0edf8beb7bc0dbff3792396b
Shegofa <shogofadeveloper12@gmail.com>
2024-08-24 10:48:48 +0400

    counter css and js added

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,8 +1,15 @@
-import "./counter.css";
+import { useState } from "react";
+import Button from "./Button";
+import "./Counter.css";
 
 const Counter = () => {
-  return (
+  const [count, setCount] = useState(0);
 
+  const increaseHandler = () => setCount(count + 1);
+  const decreaseHandler = () => setCount(count - 1);
+  const resetHandler = () => setCount(0);
+
+  return (
   );
 };
 
