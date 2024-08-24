348e882b7326618b7d0cc854c20eab2434bd7946
Shegofa <shogofadeveloper12@gmail.com>
2024-08-24 11:48:29 +0400

    using props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,17 @@
 import './Button.css';
+import PropTypes from 'prop-types';
 
 function Button({ text, clickHandler }) {
   return (
-    <button type='button' onClick={clickHandler} className={text}>
+    <button type="button" onClick={clickHandler} className={text}>
       {text}
     </button>
   );
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default Button;
